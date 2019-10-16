7f67d6cc89afd59ec77b2d0a29038c75edb512b3
taruc <cshong199@hotmail.com>
2019-10-16 15:04:05 +0800

    pratical 1 first commit

--- app/src/main/java/com/example/practical1/MainActivity.kt
+++ app/src/main/java/com/example/practical1/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.practical1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.practical1.R.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(layout.activity_main)
+
+        val rollButton : Button = findViewById(R.id.roll_button1)
+        rollButton.setOnClickListener{rollDice()}
+
+
+
+    }
+
+    private fun rollDice(){
+        val randomInt = Random.nextInt(6)+1;
+
+        val resultText: TextView = findViewById(R.id.text1)
+        resultText.text = randomInt.toString();
+
     }
 }
