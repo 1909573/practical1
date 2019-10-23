cffb16e6d5ef430c76efdd6c51915d9b0bccf7c8
taruc <cshong199@hotmail.com>
2019-10-23 14:42:34 +0800

    commited

--- app/src/main/java/com/example/practical1/MainActivity.kt
+++ app/src/main/java/com/example/practical1/MainActivity.kt
@@ -12,16 +12,18 @@ import kotlin.random.Random
 class MainActivity : AppCompatActivity() {
 
 
-    lateinit var diceImage1 : ImageView
-    lateinit var diceImage2 : ImageView
-    lateinit var diceImage3 : ImageView
+    lateinit var diceImage1: ImageView
+    lateinit var diceImage2: ImageView
+    lateinit var diceImage3: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout.activity_main)
 
-        val rollButton : Button = findViewById(R.id.roll_button1)
-        rollButton.setOnClickListener{rollDice()}
+        val rollButton: Button = findViewById(R.id.roll_button1)
+        rollButton.setOnClickListener { rollDice() }
+
+
 
         diceImage1 = findViewById(R.id.dice1)
         diceImage2 = findViewById(R.id.dice2)
@@ -30,17 +32,25 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun rollDice(){
-        val randomInt1 = Random.nextInt(6)+1
-        val randomInt2 = Random.nextInt(6)+1
-        val randomInt3 = Random.nextInt(6)+1
+
+    private fun rollDice() {
+        diceImage1.setImageResource(getRandom())
+        diceImage2.setImageResource(getRandom())
+        diceImage3.setImageResource(getRandom())
+
+
+    }
+
+
+
+    private fun getRandom(): Int {
+        val randomInt1 = Random.nextInt(6) + 1
 
         //val diceImage: ImageView = findViewById(R.id.dice)
         //resultText.text = randomInt.toString();
-        val total : TextView = findViewById(R.id.totalNumber)
-        total.text = "Total number is " + (randomInt1 + randomInt2 + randomInt3).toString()
 
-        val changeDice1 = when (randomInt1){
+
+        val changeDice1 = when (randomInt1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -49,26 +59,6 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        val changeDice2 = when (randomInt2){
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-        val changeDice3 = when (randomInt3){
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-
-        diceImage1.setImageResource(changeDice1)
-        diceImage2.setImageResource(changeDice2)
-        diceImage3.setImageResource(changeDice3)
-
+        return changeDice1
     }
 }
